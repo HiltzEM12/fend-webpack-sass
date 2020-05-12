bbc7bc519335a3c270f944fdb5ade4bf94748c42
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-12 13:35:10 -0400

    There is an weaterh API call in this one

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,8 +6,15 @@ function handleSubmit(event) {
 
     Client.checkForName(formText)
 
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+    const apiKey = '482619460b0b5529ab4cde59127b0ac2'
+
+    let url = createWeatherURL(baseURL, formText, apiKey);
+    callWeatherAPI(url);
+
+
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8080/test')
     .then(res => {
         return res.json()
     })
@@ -17,3 +24,27 @@ function handleSubmit(event) {
 }
 
 export { handleSubmit }
+
+//Wrapper to create the URL to call the weather API
+function createWeatherURL(baseURL, zipCodeField, apiKey) {
+    return baseURL + zipCodeField + '&units=imperial&appid=' + apiKey
+};
+
+
+// Function for handling the API call to the weather site
+// Just returns the temperature and a number
+async function callWeatherAPI(url = '') {
+    const request = await fetch(url);
+    // Throw an error on a 404
+    if (request.status === 404) throw new Error('Bad zip code');
+    try {
+        // Transform to JSON
+        const data = await request.json();
+        // Get the temperature
+        const results = document.getElementById('results');
+        results.innerHTML = "Temperature is " + data.main.temp
+        //alert("Temperature is " + data.main.temp)
+    } catch (error) {
+        console.log('error', error);
+    }
+};
